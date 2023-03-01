c5b6457e9defd9afb2b56a31f909f730391afda6
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-03-01 18:05:55 +0200

    Step Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
+// import { Form } from 'formik';
 import { Component } from 'react';
-// import { Container } from "./App.styled"; 
+import { Container } from "./App.styled"; 
 
 
 export class App extends Component {
@@ -7,13 +8,33 @@ export class App extends Component {
     contacts: [],
     name: ''
   }
+
+  addContact = newContact => {
+    const arrayOfContactsName = [];
+
+    for (const contact of this.state.contacts) {
+      arrayOfContactsName.push(contact.name);
+    }
+    console.log(this.state.contacts);
+  }  
+
+  
   render() {
-    return (<input
+    return (
+      <Container>
+        
+          <h1>Phonebook</h1>
+        <h2>Contacts</h2>
+      <input
       type="text"
       name="name"
       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
       required
-    />);
+        />
+          <button type="submit" onSubmit={this.addContact} >Add contact</button> 
+           
+    </Container>);
   }
+  
 }
