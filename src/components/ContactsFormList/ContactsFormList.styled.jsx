9173abe8187f0a483f79938da5ba233724b7db22
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-03-05 21:40:39 +0200

    Task 2 Final

--- src/components/ContactsFormList/ContactsFormList.styled.jsx
+++ src/components/ContactsFormList/ContactsFormList.styled.jsx
@@ -2,13 +2,15 @@ import styled from '@emotion/styled'
 
 const ContactsList = styled.li`
 padding-bottom: 10px;
- display: flex;
- justify-content: center;
+//  display: flex;
+align-items: center;
+text-content: center:
  gap: 15px;
  `;
 
 const FeedbackButton = styled.button`
-  width: 60px;
+  
+width: 60px;
   border-radius: 4px;
   font-size: 14px;
   padding: 2px 0;
